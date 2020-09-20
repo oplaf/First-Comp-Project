a26b828990e73d614335de9e9befbc3ce78da20a
Owen Lafont <owenlafont@gmail.com>
2020-09-20 16:53:30 -0400

    project

---
+++ Project1.py
@@ -0,0 +1,24 @@
+
+
+def main():
+
+
+    name = input("What is your name?:")
+    number = int(input("And can you give me a number 1:10?"))
+
+
+
+    outcome=f"Welcome to Comp151 {name}!\n"
+
+
+
+    display = outcome * number
+
+
+
+
+
+
+    print(display)
+
+main()
